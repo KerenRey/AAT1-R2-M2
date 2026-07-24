9a2157728a01c8f825183554a0c7a6ac5a263b93
josebarillas99 <josebarillas825@gmail.com>
2026-07-24 03:06:58 -0600

    Agregada pantalla 404 animada con avión y botón de compra boletos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './pages/home/home';
 import { ListaDePaises } from './components/lista-de-paises/lista-de-paises';
 import { AcercaDe } from './pages/acerca-de/acerca-de';
 import { PageListaPaises } from './pages/page-lista-paises/page-lista-paises';
+import { Pagina404 } from './pages/pagina404/pagina404';//agregado jose barillas//*/*/
 
 export const routes: Routes = [
     {
@@ -15,7 +16,11 @@ export const routes: Routes = [
     },
    { path: 'AcercaDe',
     component: AcercaDe,
-   }
+   },
+   {    // agregado jose barillas
+    path: '**',
+    component: Pagina404
+  }//------//-------//
 
 
 ];
--- src/app/pages/pagina404/pagina404.ts
+++ src/app/pages/pagina404/pagina404.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina404',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './pagina404.html',
-  styleUrl: './pagina404.css',
+  styleUrl: './pagina404.css'
 })
 export class Pagina404 {}
